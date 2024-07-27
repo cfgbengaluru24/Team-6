7d567c709c2f679216dbc70845949fb7e58fd90e
Deeksha <deeksha.cs21@bmsce.ac.in>
2024-07-27 22:32:59 +0530

    Learning page done and proper nav bar

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes.js");
 const videoRoutes = require("./routes/videoRoutes.js");
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    credentials:true,
+    origin: "*"
+  }));
 // 1) MIDDLEWARES
 
 // 2) ROUTES
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import './navbar.css';
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary color_nav">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="/">Home</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar" >
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">Home</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <Link class="nav-link active" aria-current="page" to="/learn">Learn</Link>
-        <Link class="nav-link" to="/assess">Assessment</Link>
-        <Link class="nav-link" to="/contact">Contact</Link>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link className="nav-link active" aria-current="page" to="/learn">Learn</Link>
+        <Link className="nav-link" to="/assess">Assessment</Link>
+        <Link className="nav-link" to="/contact">Contact</Link>
       </div>
     </div>
   </div>
