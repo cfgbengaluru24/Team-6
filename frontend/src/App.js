e106f06e82db1612101462101dd305bda8637d09
Deeksha <deeksha.cs21@bmsce.ac.in>
2024-07-27 18:13:16 +0530

    nav bar basic and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 
 import './App.css';
+import About from './components/about';
+import NavBar from './components/navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Learning from './components/learning';
+import Assessment from './components/assessment';
+import Contact from './components/contact';
 
 function App() {
   return (
-   <div className="mt-10 border">
-    div element
-   </div>
+    <div className="App">
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route exact path="/" element={<About/>}/>
+      <Route exact path="/learn" element={<Learning/>}/>
+      <Route exact path="/assess" element={<Assessment/>}/>
+      <Route exact path="/contact" element={<Contact/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+  </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
